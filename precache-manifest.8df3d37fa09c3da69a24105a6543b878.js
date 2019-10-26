ddc4f21abf5d0b42045262d8830f67a1486aff3c
Jinseo <rinndash@gmail.com>
2019-10-26 13:53:01 +0900

    Updates

---
+++ precache-manifest.8df3d37fa09c3da69a24105a6543b878.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d6e06a3d3432e1da94318a145eb1d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4f0b66ba0c7f328d376",
+    "url": "/static/css/main.e1ce7557.chunk.css"
+  },
+  {
+    "revision": "e980374aa066650229c0",
+    "url": "/static/js/2.a3ce6b39.chunk.js"
+  },
+  {
+    "revision": "b4f0b66ba0c7f328d376",
+    "url": "/static/js/main.c3ae95f0.chunk.js"
+  },
+  {
+    "revision": "434872893da42ccf6f6f",
+    "url": "/static/js/runtime-main.9f9cf6c2.js"
+  }
+]);
