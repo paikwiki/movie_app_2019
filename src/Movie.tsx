f6653ac382c55681657ccbf8a057f85cd4eb96c3
Jinseo <rinndash@gmail.com>
2019-10-26 12:11:46 +0900

    :sparkles: 장르를 추가

--- src/Movie.tsx
+++ src/Movie.tsx
@@ -6,16 +6,20 @@ type MovieProp = {
     year: number,
     title: string,
     summary: string,
-    poster: string
+    poster: string,
+    genres: Array<string>
 }
 
-export default function Movie({ id, year, title, summary, poster }: MovieProp): JSX.Element {
+export default function Movie({ id, year, title, summary, poster, genres }: MovieProp): JSX.Element {
     return (
         <div className="movie">
             <img src={poster} alt={title} title={title} />
             <div className="movie__data">
                 <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
+                <ul className="genres">
+                    {genres.map((genre, index) => (<li key={index} className="genres__genre">{genre}</li>))}
+                </ul>
                 <p className="movie__summary">{summary}</p>
             </div>
         </div>
